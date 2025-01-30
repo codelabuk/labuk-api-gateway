8e44374fc16efde952e96ce065470c90906c3252
afzal232 <afzal232@gmail.com>
2025-01-30 08:51:37 +0530

    Init change for api gateway

---
+++ src/main/java/com/labukapi/gateway/LabukApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.labukapi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LabukApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LabukApiGatewayApplication.class);
+    }
+}
